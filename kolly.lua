fe1b90b927138a319098a07e982a91b321b785cc
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-03-19 12:32:12 +0100

    Update kolly.lua

--- kolly.lua
+++ kolly.lua
@@ -119,7 +119,7 @@ end
 uwuSettings:Slider({Name = "Tracer Prediction",
 Minimum = 0.001,
 Maximum = 10,
-Default = 0.210,
+Default = 0.205,
 Decimals = 0.001,
 Pointer = "AimbotMain_Booty",
 Callback = function(v)
