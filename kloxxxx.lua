9324b863fcabc37c591c76dcbee58be0f5b0a911
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-03-26 01:53:19 +0100

    Update kloxxxx.lua

--- kloxxxx.lua
+++ kloxxxx.lua
@@ -85,7 +85,7 @@ end
 uwuSettings:Slider({Name = "Smoothness",
 Minimum = 0.001,
 Maximum = 1.5,
-Default = 0.027,
+Default = 0.026,
 Decimals = 0.001,
 Pointer = "AimbotMain_Smoothness",
 Callback = function(v)
