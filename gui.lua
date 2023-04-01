b617dda40a9c3cce7950c109d3294de5901d8abf
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-04-01 17:51:45 +0200

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,28 @@
+-- Table to store user IDs and their respective keys
+local keyTable = {
+    ["11983311"] = "KLOXOWNER",
+    ["352840804"] = "lqzMmRsTOo",
+}
+ 
+-- Function to check if the provided key matches the user's key
+local function checkKey(userId, key)
+    return keyTable[userId] == key
+end
+ 
+-- Get the local player's UserId
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+local userId = tostring(localPlayer.UserId)
+ 
+-- Replace "YourKey" with the key you want to use for the specific user
+local key = "MeLlamoWilson"
+ 
+-- Check if the key is correct
+if checkKey(userId, key) then
+    -- The key is correct, execute the script here
+    print("Correct key, executing script...")   
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenxdkd/test/main/kloxwithavchanger.lua"))()
+else
+    -- The key is incorrect, kick the user from the game
+    localPlayer:Kick("Incorrect key")
+end
