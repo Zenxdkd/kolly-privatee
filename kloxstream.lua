d0afed60615b8a587e3cba0e834fcc4d155b0a74
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-04-16 10:59:21 +0200

    Update kloxstream.lua

--- kloxstream.lua
+++ kloxstream.lua
@@ -1,21 +1,22 @@
+-- //SILENT + FOV
 
 local Players, Client, Mouse, RS, Camera =
-game:GetService("Players"),
-game:GetService("Players").LocalPlayer,
-game:GetService("Players").LocalPlayer:GetMouse(),
-game:GetService("RunService"),
-game.Workspace.CurrentCamera
+    game:GetService("Players"),
+    game:GetService("Players").LocalPlayer,
+    game:GetService("Players").LocalPlayer:GetMouse(),
+    game:GetService("RunService"),
+    game.Workspace.CurrentCamera
 
 local Circle = Drawing.new("Circle")
 Circle.Color = Color3.new(1,1,1)
 Circle.Thickness = 1
 
 local UpdateFOV = function ()
-    if not (Circle) then
+    if (not Circle) then
         return Circle
     end
-    Circle.Visible = getgenv().KLOX.FOV["Visible"]
-    Circle.Radius = getgenv().KLOX.FOV["Radius"] * 3
+    Circle.Visible = nukeradio.FOV["Visible"]
+    Circle.Radius = nukeradio.FOV["Radius"] * 3
     Circle.Position = Vector2.new(Mouse.X, Mouse.Y)
     return Circle
 end
@@ -23,68 +24,68 @@ end
 RS.Heartbeat:Connect(UpdateFOV)
 
 ClosestPlrFromMouse = function()
-local Target, Closest = nil, 1/0
+    local Target, Closest = nil, 1/0
+ 
+    for _ ,v in pairs(Players:GetPlayers()) do
+        if (v.Character and v ~= Client and v.Character:FindFirstChild("HumanoidRootPart")) then
+            local Position, OnScreen = Camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+            local Distance = (Vector2.new(Position.X, Position.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
 
-for _ ,v in pairs(Players:GetPlayers()) do
-    if (v.Character and v ~= Client and v.Character:FindFirstChild("HumanoidRootPart")) then
-        local Position, OnScreen = Camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
-        local Distance = (Vector2.new(Position.X, Position.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
-
-        if (Circle.Radius > Distance and Distance < Closest and OnScreen) then
-            Closest = Distance
-            Target = v
-        end
-    end
-end
-return Target
-end
-
-local WTS = function (Object)
-local ObjectVector = Camera:WorldToScreenPoint(Object.Position)
-return Vector2.new(ObjectVector.X, ObjectVector.Y)
-end
-
-local IsOnScreen = function (Object)
-local IsOnScreen = Camera:WorldToScreenPoint(Object.Position)
-return IsOnScreen
-end
-
-local FilterObjs = function (Object)
-if string.find(Object.Name, "Gun") then
-    return
-end
-if table.find({"Part", "MeshPart", "BasePart"}, Object.ClassName) then
-    return true
-end
-end
-
-local GetClosestBodyPart = function (character)
-local ClosestDistance = 1/0
-local BodyPart = nil
-if (character and character:GetChildren()) then
-    for _,  x in next, character:GetChildren() do
-        if FilterObjs(x) and IsOnScreen(x) then
-            local Distance = (WTS(x) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
-            if (Circle.Radius > Distance and Distance < ClosestDistance) then
-                ClosestDistance = Distance
-                BodyPart = x
+            if (Circle.Radius > Distance and Distance < Closest and OnScreen) then
+                Closest = Distance
+                Target = v
             end
         end
     end
+    return Target
 end
-return BodyPart
+
+local WTS = function (Object)
+    local ObjectVector = Camera:WorldToScreenPoint(Object.Position)
+    return Vector2.new(ObjectVector.X, ObjectVector.Y)
+end
+
+local IsOnScreen = function (Object)
+    local IsOnScreen = Camera:WorldToScreenPoint(Object.Position)
+    return IsOnScreen
+end
+
+local FilterObjs = function (Object)
+    if string.find(Object.Name, "Gun") then
+        return
+    end
+    if table.find({"Part", "MeshPart", "BasePart"}, Object.ClassName) then
+        return true
+    end
+end
+
+local GetClosestBodyPart = function (character)
+    local ClosestDistance = 1/0
+    local BodyPart = nil
+    if (character and character:GetChildren()) then
+        for _,  x in next, character:GetChildren() do
+            if FilterObjs(x) and IsOnScreen(x) then
+                local Distance = (WTS(x) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
+                if (Circle.Radius > Distance and Distance < ClosestDistance) then
+                    ClosestDistance = Distance
+                    BodyPart = x
+                end
+            end
+        end
+    end
+    return BodyPart
 end
 
 local Prey
 
 task.spawn(function ()
-while task.wait() do
-    if Prey then
-        if getgenv().KLOX.Silent.Enabled and getgenv().KLOX.Silent.ClosestPart == true then
-            getgenv().KLOX.Silent["Part"] = tostring(GetClosestBodyPart(Prey.Character))
+    while task.wait() do
+        if Prey then
+            if nukeradio.Silent.Enabled and nukeradio.Silent.ClosestPart == true then
+                nukeradio.Silent["Part"] = tostring(GetClosestBodyPart(Prey.Character))
+            end
         end
     end
-end
 end)
 
 local grmt = getrawmetatable(game)
@@ -92,19 +93,21 @@ local backupindex = grmt.__index
 setreadonly(grmt, false)
 
 grmt.__index = newcclosure(function(self, v)
-if (getgenv().KLOX.Silent.Enabled and Mouse and tostring(v) == "Hit") then
+    if (nukeradio.Silent.Enabled and Mouse and tostring(v) == "Hit") then
 
-    Prey = ClosestPlrFromMouse()
+        Prey = ClosestPlrFromMouse()
 
-    if Prey then
-        local endpoint = game.Players[tostring(Prey)].Character[getgenv().KLOX.Silent["Part"]].CFrame + (
-            game.Players[tostring(Prey)].Character[getgenv().KLOX.Silent["Part"]].Velocity * getgenv().KLOX.Silent.Pred
-        )
-        return (tostring(v) == "Hit" and endpoint)
+        if Prey then
+            local endpoint = game.Players[tostring(Prey)].Character[nukeradio.Silent["Part"]].CFrame + (
+                game.Players[tostring(Prey)].Character[nukeradio.Silent["Part"]].Velocity * nukeradio.Silent.Pred
+            )
+            return (tostring(v) == "Hit" and endpoint)
+        end
     end
-end
-return backupindex(self, v)
+    return backupindex(self, v)
 end)
+
+-- // Tracer/Tracer + UnlockOnDeath + shake + shake value here
 
 local CC = game.Workspace.CurrentCamera
 local Mouse = game.Players.LocalPlayer:GetMouse()
@@ -112,9 +115,9 @@ local Plr
 
 
 Mouse.KeyDown:Connect(function(Key)
-    local Keybind = getgenv().KLOX.Tracer.Key:lower()
+    local Keybind = getgenv().nukeradio.Tracer.Key:lower()
     if (Key == Keybind) then
-        if getgenv().KLOX.Tracer.Enabled == true then
+        if getgenv().nukeradio.Tracer.Enabled == true then
             IsTargetting = not IsTargetting
             if IsTargetting then
                 Plr = GetClosest()
@@ -193,77 +196,140 @@ function GetNearestPartToCursorOnCharacter(character)
 end
 
 game.RunService.Heartbeat:Connect(function()
-    if getgenv().KLOX.Tracer.Enabled == true and Plr and Plr.Character ~= nil then
-        if getgenv().KLOX.Misc.UnlockedOnDeath then
+    if getgenv().nukeradio.Tracer.Enabled == true and Plr and Plr.Character ~= nil then
+        if getgenv().nukeradio.Misc.UnlockedOnDeath then
             if Plr.Character.BodyEffects["K.O"].Value then Plr = nil end
         end
-        if getgenv().KLOX.Misc.Shake then
-            local Main = CFrame.new(Camera.CFrame.p,Plr.Character[getgenv().KLOX.Tracer.Part].Position + Plr.Character[getgenv().KLOX.Tracer.Part].Velocity * getgenv().KLOX.Tracer.Pred +
+        if getgenv().nukeradio.Misc.Shake then
+            local Main = CFrame.new(Camera.CFrame.p,Plr.Character[getgenv().nukeradio.Tracer.Part].Position + Plr.Character[getgenv().nukeradio.Tracer.Part].Velocity * getgenv().nukeradio.Tracer.Pred +
             Vector3.new(
-                math.random(-getgenv().KLOX.Misc.ShakeValue, getgenv().KLOX.Misc.ShakeValue),
-                math.random(-getgenv().KLOX.Misc.ShakeValue, getgenv().KLOX.Misc.ShakeValue),
-                math.random(-getgenv().KLOX.Misc.ShakeValue, getgenv().KLOX.Misc.ShakeValue)
+                math.random(-getgenv().nukeradio.Misc.ShakeValue, getgenv().nukeradio.Misc.ShakeValue),
+                math.random(-getgenv().nukeradio.Misc.ShakeValue, getgenv().nukeradio.Misc.ShakeValue),
+                math.random(-getgenv().nukeradio.Misc.ShakeValue, getgenv().nukeradio.Misc.ShakeValue)
             ) * 0.1)
-            Camera.CFrame = Camera.CFrame:Lerp(Main, getgenv().KLOX.Tracer.Smoothness, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
+            Camera.CFrame = Camera.CFrame:Lerp(Main, getgenv().nukeradio.Tracer.Smoothness, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
         else
-            local Main = CFrame.new(Camera.CFrame.p,Plr.Character[getgenv().KLOX.Tracer.Part].Position + Plr.Character[getgenv().KLOX.Tracer.Part].Velocity * getgenv().KLOX.Tracer.Pred)
-            Camera.CFrame = Camera.CFrame:Lerp(Main, getgenv().KLOX.Tracer.Smoothness, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
+            local Main = CFrame.new(Camera.CFrame.p,Plr.Character[getgenv().nukeradio.Tracer.Part].Position + Plr.Character[getgenv().nukeradio.Tracer.Part].Velocity * getgenv().nukeradio.Tracer.Pred)
+            Camera.CFrame = Camera.CFrame:Lerp(Main, getgenv().nukeradio.Tracer.Smoothness, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
         end
     end
 end)
 
 task.spawn(function()
     while task.wait() do
-        if getgenv().KLOX.Tracer.Enabled and Plr ~= nil and (Plr.Character) then
-            getgenv().KLOX.Tracer.Part = tostring(GetNearestPartToCursorOnCharacter(Plr.Character))
+        if getgenv().nukeradio.Tracer.Enabled and Plr ~= nil and (Plr.Character) then
+            getgenv().nukeradio.Tracer.Part = tostring(GetNearestPartToCursorOnCharacter(Plr.Character))
         end
     end
 end)
 
--- resolver
 
-RS.Heartbeat:Connect(function()
-    if  getgenv().KLOX.Resolver.Enabled then
-        pcall(function()
-            for i,v in pairs(game.Players:GetChildren()) do
-                if v ~= game.Players.LocalPlayer  then
-                    local hrp = v.Character.HumanoidRootPart
-                    hrp.Velocity =  Vector3.new(0.1,-0,0.34) *(0.11^00)
-                    hrp.AssemblyLinearVelocity = Vector3.new(0,0,0) *(0^0)
-                    hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
-                    hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z) 
-                end
+
+-- //Resolver + wallcheck here
+
+local WallCheck = function(destination, ignore)
+    if getgenv().nukeradio.Misc.WallCheck then
+        local Origin = Camera.CFrame.p
+        local CheckRay = Ray.new(Origin, destination - Origin)
+        local Hit = game.workspace:FindPartOnRayWithIgnoreList(CheckRay, ignore)
+        return Hit == nil
+    else
+        return true
+    end
+end
+
+GetClosestToMouse = function()
+    local Target, Closest = nil, 1 / 0
+
+    for _, v in pairs(Players:GetPlayers()) do
+        if (v.Character and v ~= Client and v.Character:FindFirstChild("HumanoidRootPart")) then
+            local Position, OnScreen = Camera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+            local Distance = (Vector2.new(Position.X, Position.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
+
+            if
+                (Circle.Radius > Distance and Distance < Closest and OnScreen and
+                    WallCheck(v.Character.HumanoidRootPart.Position, {Client, v.Character}))
+             then
+                Closest = Distance
+                Target = v
             end
-        end)
+        end
+    end
+    return Target
+end
+
+if  getgenv().nukeradio.Resolver.Enabled then
+    shared.lastpos = nil
+shared.velocity = nil
+RS.Heartbeat:connect(function(step)
+    if getgenv().Nemesis.Resolver.Enabled and Nemesishook then
+        local lastPos = shared.lastpos
+        if not lastPos or lastPos == nil then
+            shared.lastpos = Nemesishook.Character:GetPivot().p
+            return
+        end
+        local char = Nemesishook.Character
+
+        
+        shared.velocity = (Nemesishook.Character:GetPivot().p - lastPos) / step
+        shared.lastpos = Nemesishook.Character:GetPivot().p
     end
 end)
 
 
+-- //Macro here
 
-
---macro
 
 local Player = game:GetService("Players").LocalPlayer
-            local Mouse = Player:GetMouse()
-            local SpeedGlitch = false
-            Mouse.KeyDown:Connect(function(Key)
-                if getgenv().KLOX.Macro.Enabled == true and Key == getgenv().KLOX.Macro.Keybind then
-                    SpeedGlitch = not SpeedGlitch
-                    if SpeedGlitch == true then
-                        repeat game:GetService("RunService").Heartbeat:wait()
-                            keypress(0x49)
-                            game:GetService("RunService").Heartbeat:wait()
+        local Mouse = Player:GetMouse()
+        local SpeedGlitch = false
+        Mouse.KeyDown:Connect(function(Key)
+            if getgenv().nukeradio.Macro.Type == "3Person" and getgenv().nukeradio.Macro.Enabled == true and Key == getgenv().nukeradio.Macro.Keybind then
+                SpeedGlitch = not SpeedGlitch
+                if SpeedGlitch == true then
+                    repeat game:GetService("RunService").Heartbeat:wait()
+                        keypress(0x49)
+                        game:GetService("RunService").Heartbeat:wait()
 
-                            keypress(0x4F)
-                            game:GetService("RunService").Heartbeat:wait()
+                        keypress(0x4F)
+                        game:GetService("RunService").Heartbeat:wait()
 
-                            keyrelease(0x49)
-                            game:GetService("RunService").Heartbeat:wait()
+                        keyrelease(0x49)
+                        game:GetService("RunService").Heartbeat:wait()
 
-                            keyrelease(0x4F)
-                            game:GetService("RunService").Heartbeat:wait()
+                        keyrelease(0x4F)
+                        game:GetService("RunService").Heartbeat:wait()
 
-                        until SpeedGlitch == false
-                    end
+                    until SpeedGlitch == false
                 end
-            end)
+            end
+        end)
+        
+        
+        
+        local Player = game:GetService("Players").LocalPlayer
+        local Mouse = Player:GetMouse()
+        local SpeedGlitch = false
+        Mouse.KeyDown:Connect(function(Key)
+            if getgenv().nukeradio.Macro.Type == "1Person" and getgenv().nukeradio.Macro.Enabled == true and Key == getgenv().nukeradio.Macro.Keybind then
+                SpeedGlitch = not SpeedGlitch
+                if SpeedGlitch == true then
+                    repeat game:GetService("RunService").Heartbeat:wait()
+                            game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", true, game)
+                                 game:GetService("RunService").Heartbeat:wait()
+                                 game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", false, game)
+                                 game:GetService("RunService").Heartbeat:wait()
+                    until SpeedGlitch == false
+                end
+            end
+        end)
+
+
+
+
+-- //Fps unlocker
+local fps = 1200
+ 
+    if setfpscap then
+        setfpscap(fps)
+end
